349c8122a20f9a4a040eacf972b3ab978354b140
Anastasiia <nastyabarabakh@gmail.com>
2021-09-24 21:09:51 +0300

    Done

--- JS-02/main.js
+++ JS-02/main.js
@@ -7,14 +7,10 @@ let skipEvenNumber = confirm('Should I skip even numbers?');
 let result = 0;
 
 for (let i = N; i <= M; i++) {
-    if (skipEvenNumber === false) {
-        result += i;
-    } else {
-        if (i % 2) {
-            result += i;
-        }
-
+    if (skipEvenNumber && !(i % 2)) {
+        continue;
     }
+    result += i;
 }
 
 alert(`Result: ${result}`);
