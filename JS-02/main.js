836350f371dd3a6dcb7eb18a33fdb69817b729c7
Anastasiia <nastyabarabakh@gmail.com>
2021-09-27 01:37:05 +0300

    redone

--- JS-02/main.js
+++ JS-02/main.js
@@ -1,16 +1,19 @@
-let N = parseInt(prompt('Enter the number N:'));
-
-let M = parseInt(prompt('Enter the number M:'));
-
+let N = checkNumber('N');
+let M = checkNumber('M');
 let skipEvenNumber = confirm('Should I skip even numbers?');
-
 let result = 0;
-
 for (let i = N; i <= M; i++) {
     if (skipEvenNumber && !(i % 2)) {
         continue;
     }
     result += i;
 }
-
 alert(`Result: ${result}`);
+
+function checkNumber(name) {
+    let num = +prompt('Enter the number ' + name + ':');
+    while (!Number.isInteger(num)) {
+        num = +prompt('Enter the NUMBER ' + name + ': (ex.: 1, 2, 7, 15)');
+    }
+    return num;
+}
