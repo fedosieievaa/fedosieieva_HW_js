a24fb560a4a88f1db039475d7d0573dd0f28bb43
Anastasiia <nastyabarabakh@gmail.com>
2021-09-27 15:20:54 +0300

    Added N > M verification

--- JS-02/main.js
+++ JS-02/main.js
@@ -1,6 +1,11 @@
 let N = checkNumber('N');
 let M = checkNumber('M');
-let skipEvenNumber = confirm('Should I skip even numbers?');
+const skipEvenNumber = confirm('Should I skip even numbers?');
+if (N > M) {
+    let temporary = N;
+    N = M;
+    M = temporary;
+}
 let result = 0;
 for (let i = N; i <= M; i++) {
     if (skipEvenNumber && !(i % 2)) {
