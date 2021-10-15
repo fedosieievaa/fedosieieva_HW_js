ab000c8e4356d37744f075618fd108fee6a75984
fedosieievaa <nastyabarabakh@gmail.com>
2021-10-15 17:06:07 +0300

    JS-08 done

---
+++ JS-08/main.js
@@ -0,0 +1,31 @@
+const ukraine = { tax: 0.195, middleSalary: 1789, vacancies: 11476 };
+const latvia = { tax: 0.25, middleSalary: 1586, vacancies: 3921 };
+const litva = { tax: 0.15, middleSalary: 1509, vacancies: 1114 };
+
+// 1
+const getMyTaxes = function(salary) {
+    return +(this.tax * salary).toFixed(2);
+}
+console.log('Скільки податків ви заплатите як IT-спеціаліст в якійсь з країн: ' + getMyTaxes.call(ukraine, 1000));
+
+// 2
+const getMiddleTaxes = function() {
+    return +(this.tax * this.middleSalary).toFixed(2);
+}
+console.log('Скільки у середньому податків платять IT-спеціалісти у певній країні: ' + getMiddleTaxes.call(ukraine));
+
+// 3
+const getTotalTaxes = function() {
+    return +(this.tax * this.middleSalary * this.vacancies).toFixed(2);
+}
+console.log('Скільки всього податків платять IT-спеціалісти у певній країні: ' + getTotalTaxes.call(ukraine));
+
+// 4
+const getMySalary = function() {
+    const result = {};
+    result.salary = +(Math.random() * (2000 - 1500) + 1500).toFixed(2);
+    result.taxes = this.tax;
+    result.profit = +(this.middleSalary - (this.tax * result.salary)).toFixed(2);
+    return result;
+}
+setInterval(() => console.log(getMySalary.call(ukraine)), 10000);
