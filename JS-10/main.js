edcb77d072799e225a984662b9a075d837f050cd
fedosieievaa <nastyabarabakh@gmail.com>
2021-10-28 22:59:02 +0300

    Improved functions

--- JS-10/main.js
+++ JS-10/main.js
@@ -1,11 +1,17 @@
+const container = document.getElementById('boxes');
 const generateBlocks = () => {
-    for (const box of document.getElementById('boxes').children) {
-        box.style.backgroundColor = "#" + Math.floor(Math.random() * 0xFFFFFF).toString(16);
+    for (let i = 0; i < 25; i++) {
+        const div = document.createElement('div')
+        container.append(div)
+        div.style.backgroundColor = "#" + Math.floor(Math.random() * 0xFFFFFF).toString(16);
     }
 }
 generateBlocks()
 
 const generateBlocksInterval = () => {
-    setInterval(() => generateBlocks(), 1000);
+    setInterval(() => {
+        container.innerHTML = ''
+        generateBlocks()
+    }, 1000);
 }
 generateBlocksInterval()
