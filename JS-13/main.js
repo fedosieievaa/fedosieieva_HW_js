7895ee78f7fd9b39bbb8ee07b0bb24eb804bf26b
fedosieievaa <nastyabarabakh@gmail.com>
2021-11-02 15:08:43 +0200

    JS-13

---
+++ JS-13/main.js
@@ -0,0 +1,25 @@
+const getRandomChinese = function(length) {
+    return new Promise((resolve, reject) => {
+        let sign;
+        let arr = [];
+        let i = 0;
+        const interval = setInterval(() => {
+            sign = String(Date.now()).substr(8);
+            arr.push(String.fromCharCode(sign));
+            if (i === length - 1) {
+                clearInterval(interval);
+            }
+            i++;
+        }, 50);
+        setTimeout(() => {
+            resolve(arr);
+        }, length * 50)
+    })
+}
+console.log('Start time: ' + new Date().getMilliseconds() + 'ms');
+getRandomChinese(4).then((data) => {
+    return data.join('');
+}).then((data) => {
+    console.log(data);
+    console.log('Finish time: ' + new Date().getMilliseconds() + 'ms');
+})
