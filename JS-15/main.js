7142c47c0ec9d7e3d81611ef977226b72d5033f0
fedosieievaa <nastyabarabakh@gmail.com>
2021-11-09 18:27:11 +0200

    JS-15 done

---
+++ JS-15/main.js
@@ -0,0 +1,40 @@
+const generatorBtn = document.getElementById('generator-btn');
+const listOfIds = document.getElementById('id-generator');
+
+const createIdGenerator = function*() {
+    let i = 1;
+    while (true) {
+        yield i++;
+    }
+}
+const id = createIdGenerator();
+
+generatorBtn.addEventListener('click', () => {
+    listOfIds.innerHTML += `<li class="id-item">${id.next().value}</li>`
+})
+
+const up = document.getElementById('up');
+const down = document.getElementById('down');
+const textContainer = document.getElementById('fonts');
+
+const newFontGenerator = function*(font) {
+    let size = yield font;
+    while (true) {
+        if (size === 'up') {
+            size = font += 2;
+        }
+        if (size === 'down') {
+            size = font -= 2;
+        }
+        size = yield font;
+    }
+}
+
+const fontGenerator = newFontGenerator(14);
+
+up.addEventListener('click', () => {
+    textContainer.style.fontSize = `${fontGenerator.next('up').value}px`
+});
+down.addEventListener('click', () => {
+    textContainer.style.fontSize = `${fontGenerator.next('down').value}px`
+});
